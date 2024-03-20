815c3db42ed5981107c56c684877c684f3d97160
Dharmendra Choudhary <107131075+Hunter47dj@users.noreply.github.com>
2024-03-20 22:33:07 +0530

    Updated Solution - GfG to GitHub

--- Medium/Sum of nodes on the longest path from root to leaf node/sum-of-nodes-on-the-longest-path-from-root-to-leaf-node.py
+++ Medium/Sum of nodes on the longest path from root to leaf node/sum-of-nodes-on-the-longest-path-from-root-to-leaf-node.py
@@ -33,8 +33,6 @@ class Solution:
                 queue.append([node.right, length + 1, current_sum + node.right.data])
         
         return max_sum
-
-
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
