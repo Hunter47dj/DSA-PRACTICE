e9adfaf0d9e1e3a5ba3fe87d9e5dcec9b36b7509
Dharmendra Choudhary <107131075+Hunter47dj@users.noreply.github.com>
2024-05-13 23:07:35 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of Good Components/number-of-good-components.cpp
+++ Medium/Number of Good Components/number-of-good-components.cpp
@@ -31,6 +31,7 @@ class Solution {
   public:
     int findNumberOfGoodComponent(int e, int v, vector<vector<int>> &edges) {
         // code here
+        
          vector<set<int>> vertices(v);
         for(int i = 0; i < v; i++) vertices[i].insert(i);
         for(auto edge : edges) {
