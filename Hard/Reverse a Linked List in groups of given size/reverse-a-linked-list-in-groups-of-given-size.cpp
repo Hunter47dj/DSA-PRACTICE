a232325142ef5dd7883c968891be7e4e6abf0f75
Dharmendra Choudhary <107131075+Hunter47dj@users.noreply.github.com>
2024-06-05 10:14:56 +0530

    Updated Solution - GfG to GitHub

--- Hard/Reverse a Linked List in groups of given size/reverse-a-linked-list-in-groups-of-given-size.cpp
+++ Hard/Reverse a Linked List in groups of given size/reverse-a-linked-list-in-groups-of-given-size.cpp
@@ -49,54 +49,63 @@ void printList(struct node *node)
 class Solution
 {
     public:
+    int getlength(node* head)
+    {
+        node* temp = head;
+        int cnt= 0;
+        while (temp!= NULL)
+        {
+            temp = temp->next;
+            cnt++;
+        }
+        return cnt;    
+    }
+
+
     struct node *reverseIt (struct node *head, int k)
     { 
         // Complete this method
-        node* temp = head;
-        int size = 0;
-        while(temp!= NULL)
-        {
-            temp = temp->next;
-            size++;
-        }
-        
-        // base case
         if(head == NULL)
-            return NULL;
+    {
+        return head;
+    }
+
+    int sizeOfLL = getlength(head);
+
+    if (sizeOfLL < k)
+    {
+        k = sizeOfLL;  // reversing the remaining elements at end by treating them as one group
+
+    }
+    
+    // step1- reverse the  k nodes
+    node* prev = NULL;
+    node* curr = head;
+    node* next = NULL;
+
+    int cnt =0;
+    while (curr != NULL && cnt <k)
+    {
+        next = curr->next;
+        curr->next = prev;
+
+        prev = curr;
+        curr = next;
+
+        cnt++;
+    }
+    
+    //step2- recurssion se head le aao
+    if(next!= NULL)
+    {
+
+    head->next = reverseIt(next,k);
+    }
+
+    // return the head of the reversed node i.e. prev.
+
+    return prev;
         
-        if(size< k)
-        {
-            k  = size;
-        }
-        
-        // step1 - reverse the k nodes
-        
-        node* prev = NULL;
-        node* curr = head;
-        node* next = NULL;
-        int cnt = 0;
-        
-        while(curr != NULL && cnt < k)
-        {
-            next = curr->next;
-            curr->next = prev;
-            
-            prev = curr;
-            curr = next;
-            
-            cnt++;
-        }
-        
-        // step2- recurssion se head lelo
-        
-        if(next != NULL)
-        {
-            head->next = reverseIt(next, k);
-        }
-        
-        //step-3 return krwao reversed list ka head i.e. prev
-        
-        return prev;
     }
 };
 
