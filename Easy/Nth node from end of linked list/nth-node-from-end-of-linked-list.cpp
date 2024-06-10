440f87886b09ab6f0b7c84efd63fead846a8216e
Dharmendra Choudhary <107131075+Hunter47dj@users.noreply.github.com>
2024-06-10 17:41:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Nth node from end of linked list/nth-node-from-end-of-linked-list.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+//Initial Template for C++
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+
+// } Driver Code Ends
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+class Solution{
+public:
+    int getlength(Node* head)
+    {
+        Node* temp = head;
+        int cnt =0;
+        while(temp != NULL)
+        {
+            temp =temp->next;
+            cnt++;
+            
+        }
+        return cnt;
+    }
+    
+    int getNthFromLast(Node *head, int n)
+    {
+           // Your code here
+           int len = getlength(head);
+           
+           if(len < n)
+           {
+               return -1;
+           }
+           
+           int cnt= 0;
+           Node* temp = head;
+           while(temp != NULL && cnt < len-n )
+           {
+               temp = temp->next;
+               cnt++;
+           }
+           
+           return temp->data;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+    cout<<obj.getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
